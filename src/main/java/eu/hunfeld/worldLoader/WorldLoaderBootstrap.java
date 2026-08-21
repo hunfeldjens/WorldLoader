abe99e33e759d0cd0f36e5493fa4682956944f3f
jenshunfeld <jenshunfeld12@gmail.com>
2026-08-21 16:46:04 +0200

    An New WorldLoader Project

---
+++ src/main/java/eu/hunfeld/worldLoader/WorldLoaderBootstrap.java
@@ -0,0 +1,12 @@
+package eu.hunfeld.worldLoader;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+
+class WorldLoaderBootstrap implements PluginBootstrap {
+
+    @Override
+    public void bootstrap(final BootstrapContext context) {
+        // Plugin bootstrap logic
+    }
+}
